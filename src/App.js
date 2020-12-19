e3be00edd5a7d0d3cda0b453dd3ac34eba6edbfd
sh.aghahadi <z77aghahadi@yahoo.com>
2020-12-19 23:17:30 +0330

    part1-nerkh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+const api={
+  key:"",
+  base:""
+}
 
 function App() {
   return (
-    <div className="App">
-      salam shahrbanoo azizam
+    <div className="app">
+      <main>
+        <div className="search-box">
+            <input
+            type="text"
+            className="search-bar"
+            placeholder="search..."
+            />
+           </div>
+      </main>
     </div>
   );
 }
